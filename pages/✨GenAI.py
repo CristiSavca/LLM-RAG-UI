59bc0c79437c5663cdb9bd066f5798975bfbb1bd
Cristi Savca <cristisavca44@gmail.com>
2024-02-07 13:44:37 -0500

    Updated UI

--- pages/✨GenAI.py
+++ pages/✨GenAI.py
@@ -1,51 +1,68 @@
 import streamlit as st
 from htmlTemplates import css, button_template, card_template
 from streamlit_card import card
-st.set_page_config(layout="wide")
+from streamlit_pills import pills
 
-st.title("AI Generation Tools")
+st.set_page_config(page_title="AI Generation Tools", layout="centered")
 
-col1, col2, col3, col4, col5, col6 = st.columns(6)
+# st.title("AI Generation Tools")
+# selected = pills("Tools", ["Presentation", "Flowchart", "Image"], ["🖼️", "📊", "📷"], None)
+#st.write(selected)
+
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.button("🐍Python")
+    st.button("📷Image", use_container_width=True, type="primary")
 with col2:
-    st.button("📀SQL")
+    st.button("📊Flowchart", use_container_width=True, type="primary")
 with col3:
-    st.button("📊Flowchart")
-with col4:
-    st.button("✏️Summarize")
-with col5:
-    st.button("🖼️Presentation")
-with col6:
-    st.button("📧Email")
+    st.button("🖼️Presentation", use_container_width=True, type="primary")
 
+col4, col5, col6 = st.columns([1, 1, 1], gap="large")
 
-# Cards grid
-for i in range(1, 5):
-    cols = st.columns(2)
-    cols[0] = card(
-        key=str(i),
-        title="Hello World!",
-        text="Some description",
-        image="http://placekitten.com/200/300",
-        url="https://github.com/gamcoh/st-card"
-    )
-    cols[1] = card(
-        key=str(i+10),
-        title="Streamlit Card",
-        text="This is a test card",
-        image="https://placekitten.com/500/500",
-        styles={
-            "card": {
-                "width": "500px",
-                "height": "500px",
-                "border-radius": "60px",
-                "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
-            },
-            "title": {
-                "color": "red",
-            },
-        },
-)
+# with col4:
+#     for i in range(1, 3):
+#         cols = card(
+#             key=str(i),
+#             title="Hello World!",
+#             text="Some description",
+#             image="http://placekitten.com/200/300",
+#             url="https://github.com/gamcoh/st-card"
+#         )
+#
+# with col5:
+#     for i in range(1, 3):
+#         cols = card(
+#             key=str(i + 10),
+#             title="Streamlit Card",
+#             text="This is a test card",
+#             image="https://placekitten.com/500/500",
+#             styles={
+#                 "card": {
+#                     "width": "300px",
+#                     "height": "250px",
+#                     "border-radius": "20px",
+#                     "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+#                 },
+#                 "title": {
+#                     "color": "red",
+#                 },
+#             },
+#         )
+#
+# with col6:
+#     for i in range(1, 3):
+#         cols = card(
+#             key=str(i+20),
+#             title="Hello World!",
+#             text="Some description",
+#             image="http://placekitten.com/200/300",
+#             url="https://github.com/gamcoh/st-card"
+#         )
 
+st.container(height=650, border=False)
+web_search_toggle, chat_input = st.columns([0.2, 0.8])
+with web_search_toggle:
+    st.toggle(label="Web Search")
+with chat_input:
+    user_question = st.chat_input("Message BofA-GPT...")
